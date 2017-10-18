69d11f789e3c1a673e5d5151035c2056a99c9117
Ivan Smolin <ivan.smolin@touchin.ru>
2017-10-18 18:59:10 +0300

    remove tailor

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,11 +12,9 @@ cd $PROJECT_NAME
 git submodule add --name code-quality https://github.com/TouchInstinct/code-quality-ios code-quality
 
 ln -s code-quality/.swiftlint.yml .swiftlint.yml
-ln -s code-quality/.tailor.yml .tailor.yml
 ln -s code-quality/cpd_script.php cpd_script.php
 
 git add .swiftlint.yml
-git add .tailor.yml
 git add cpd_script.php
 
 git commit --amend -m "Initial commit"
