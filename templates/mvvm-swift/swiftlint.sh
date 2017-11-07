499b857a1f7eb250af9b5c0f2119c5bce5ee7c45
Ivan Smolin <ivan.smolin@touchin.ru>
2017-11-07 14:50:47 +0300

    remove system installation SwiftLint check

--- templates/mvvm-swift/swiftlint.sh
+++ templates/mvvm-swift/swiftlint.sh
@@ -1,5 +1,1 @@
-if which swiftlint >/dev/null; then
-    ${PODS_ROOT}/SwiftLint/swiftlint
-else
-    echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-fi
+${PODS_ROOT}/SwiftLint/swiftlint
