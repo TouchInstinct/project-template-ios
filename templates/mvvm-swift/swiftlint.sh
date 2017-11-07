528c0278de5bd8ffb1311f2484596252cc133ec9
Ivan Smolin <ivan.smolin@touchin.ru>
2017-11-07 14:47:07 +0300

    add SwiftLint as pod

--- templates/mvvm-swift/swiftlint.sh
+++ templates/mvvm-swift/swiftlint.sh
@@ -1,5 +1,5 @@
 if which swiftlint >/dev/null; then
-    swiftlint
+    ${PODS_ROOT}/SwiftLint/swiftlint
 else
     echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
 fi
