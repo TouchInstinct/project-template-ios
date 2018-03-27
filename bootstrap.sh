0c0c1a76fface3ecc33682f96ea200f6b459544f
Igor Kislyuk <igorkislyuk@icloud.com>
2018-03-28 00:17:17 +0300

    Update bootstrap for xcode

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,19 +2,64 @@
 
 PROJECT_NAME=$1
 PROJECTS_PATH=$2
+CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd $PROJECTS_PATH
 
-echo $PROJECT_NAME | liftoff
-
+# main project folder
+mkdir $PROJECT_NAME
 cd $PROJECT_NAME
 
-git submodule add --name code-quality git@github.com:TouchInstinct/code-quality-ios.git
+# source code project folder
+mkdir $PROJECT_NAME
+cd $PROJECT_NAME
 
-ln -s code-quality/.swiftlint.yml .swiftlint.yml
-ln -s code-quality/cpd_script.php cpd_script.php
+for folder in `cat $CURRENT_DIR/foldernames.txt`; do
+    echo "Creating $folder ..."
+    mkdir $folder
+    touch $folder/.gitkeep
+done
 
-git add .swiftlint.yml
-git add cpd_script.php
+cd ..
 
-git commit --amend -m "Initial commit"
+# generate yml project file
+cat <<EOF >project.yml
+name: $PROJECT_NAME
+options:
+  bundleIdPrefix: ru.touchin.$PROJECT_NAME
+targets:
+  $PROJECT_NAME:
+    type: application
+    platform: iOS
+    deploymentTarget: "10.0"
+    sources: [$PROJECT_NAME]
+EOF
+
+# generate xcode project file
+xcodegen --spec project.yml
+
+# install pods
+
+# expand scripts
+
+# configure submodules
+
+# do some stuff with provision profiles
+
+# enable shared scheme
+
+
+
+# echo $PROJECT_NAME | liftoff
+#
+# cd $PROJECT_NAME
+#
+# git submodule add --name code-quality git@github.com:TouchInstinct/code-quality-ios.git
+#
+# ln -s code-quality/.swiftlint.yml .swiftlint.yml
+# ln -s code-quality/cpd_script.php cpd_script.php
+#
+# git add .swiftlint.yml
+# git add cpd_script.php
+#
+# git commit --amend -m "Initial commit"
