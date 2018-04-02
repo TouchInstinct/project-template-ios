b12b77b50073ffd8162d581f324e1d7c8eb6cc66
Igor Kislyuk <igorkislyuk@icloud.com>
2018-04-02 19:47:06 +0300

    Add localization

---
+++ sources/project/Resources/Localization/String+Localization.swift
@@ -0,0 +1,10 @@
+import Foundation
+import LeadKit
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable line_length
+// swiftlint:disable file_length
+// swiftlint:disable identifier_name
+
+extension String {
+}
