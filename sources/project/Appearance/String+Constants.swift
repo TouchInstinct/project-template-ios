beaf86b5ea25d4190811bea89ff24ae0f1cee397
Vlad <Suhomlinov.dev@gmail.com>
2020-06-09 14:37:56 +0300

    Add String Constants

---
+++ sources/project/Appearance/String+Constants.swift
@@ -0,0 +1,23 @@
+extension String {
+    static let newLine = "\n"
+
+    static let empty = ""
+
+    static let whiteSpace = " "
+
+    static let dot = "."
+
+    static let comma = ","
+
+    static let zero = "0"
+
+    static let leftSquareBracket = "["
+
+    static let rightSquareBracket = "]"
+
+    static let asterisk = "*"
+
+    static let rightArrow = "›"
+
+    static let underscore = "_"
+}
